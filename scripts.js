756e0108e65b1e7e983120423e7c26cf084665d1
Rafael Perger <rafael.perger@gmail.com>
2022-02-08 19:01:59 +0100

    Create four function for basic mathematical opreations (add, substract, multiply, divide).

---
+++ scripts.js
@@ -0,0 +1,28 @@
+let a = 2;
+let b = 2;
+
+console.log(add(a, b));
+console.log(substract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
+console.log(operate(divide, a, b));
+
+function add(a,b) {
+    return a + b;
+};
+
+function substract(a, b) {
+    return a - b;
+};
+
+function multiply (a, b) {
+    return a * b;
+};
+
+function divide (a, b) {
+    return a / b;
+};
+
+function operate(operation, a, b) {
+    return operation(a, b);
+}
